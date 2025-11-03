9ba1f8d71d26873f468fe73a3e7f1cfe89b9a9dc
Lydia Bagdon <74695306+lbagd9898@users.noreply.github.com>
2025-11-03 10:27:55 -0500

    check coords function implemented, communication with server established

--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
+app.use(cors());
+
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("fuck you");
+  res.send("server up and running");
 });
 
-console.log(process.env.DATABASE_URL);
+app.post("/check-data", (req, res) => {
+  console.log("connected to server");
+  console.log(req.body);
+  const body = req.body;
+  res.status(200).json({ body });
+});
 
 app.listen(PORT, () => {
   console.log("werking");
