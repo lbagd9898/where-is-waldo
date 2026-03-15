abb3010ca0806fcbb491f5305901269f4fa865c1
Lydia Bagdon <74695306+lbagd9898@users.noreply.github.com>
2026-03-15 08:48:30 -0400

    serve react build from express

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
 const router = require("./router");
+const path = require("path");
 
 require("dotenv").config();
 app.use(cors());
@@ -11,6 +11,11 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.use("/", router);
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("werking");
